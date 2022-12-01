a7747fe915025b983b3007358dc79bb312dbf56c
Joseph Newman <josephnew900@gmail.com>
2022-11-30 17:20:15 -0800

    Added Reddit API functionality for the "hot" page.

--- server.py
+++ server.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
 import flask
+import praw
 
+
+reddit = praw.Reddit(
+	client_id="5DARDMSn1eAPkxNksqwILQ",
+	client_secret="3Oig8HDiISuXjX3g47oQMArtmi4pAg",
+	password="Projectcsumb",
+	user_agent="script for CSUMB project",
+	username="Csumbproject",
+)
+reddit.read_only = False
+
+def redditApiSearch(reddit, word):
+	for submission in reddit.front.hot():
+		print(submission.title)
+
+#test functionality for apiSearch returns tokens; convert tokens
+redditApiSearch(reddit, "programmig")
 app = Flask(__name__,  static_folder="public", static_url_path="")
 
 @app.route('/')
