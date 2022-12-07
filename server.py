7ac76a64ad24c0d068761365411369080f6c23e6
Luke Berry <lukeberrycs@gmail.com>
2022-12-07 15:43:52 -0800

    Fixed variable names as needed, added functionality so the top 5 stories now show on the webpage

--- server.py
+++ server.py
@@ -6,60 +6,65 @@ from pprint import pprint
 import requests #required for ABC news trending
 from bs4 import BeautifulSoup #required for ABC news trending
 
-reddit = praw.Reddit(
-	client_id="5DARDMSn1eAPkxNksqwILQ",
-	client_secret="3Oig8HDiISuXjX3g47oQMArtmi4pAg",
-	password="Projectcsumb",
-	user_agent="script for CSUMB project",
-	username="Csumbproject",
-)
-reddit.read_only = False
-# documantation of PRAW is located https://praw.readthedocs.io/en/stable/getting_started/quick_start.html
-# this holds the functions of what reddit can pull and how to use the tokens provided;
-def redditApiSearch(reddit, word):
-	for submission in reddit.front.hot():
-		print(submission.title)
+##UNCOMMENT BEFORE COMMIT##
+# reddit = praw.Reddit(
+# 	client_id="5DARDMSn1eAPkxNksqwILQ",
+# 	client_secret="3Oig8HDiISuXjX3g47oQMArtmi4pAg",
+# 	password="Projectcsumb",
+# 	user_agent="script for CSUMB project",
+# 	username="Csumbproject",
+# )
+# reddit.read_only = False
+# # documantation of PRAW is located https://praw.readthedocs.io/en/stable/getting_started/quick_start.html
+# # this holds the functions of what reddit can pull and how to use the tokens provided;
+# def redditApiSearch(reddit, word):
+# 	for submission in reddit.front.hot():
+# 		print(submission.title)
 
-#test functionality for apiSearch returns tokens; convert tokens
-redditApiSearch(reddit, "tempword")
+# #test functionality for apiSearch returns tokens; convert tokens
+# redditApiSearch(reddit, "tempword")
 app = Flask(__name__,  static_folder="public", static_url_path="")
 
-@app.route('/')
-def default():
-	return flask.render_template("index.html") 
+# @app.route('/')
+# def default():
+# 	return flask.render_template("index.html") 
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+#UNCOMMENT BEFORE COMMIT##
 
 #twitter
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAKvrjQEAAAAAC7rhBPngaUoWZtiE2Po8AI7K0bM%3De9ddHzssMxizr1AxPW4EBmqwKldD8rGDLHnhtXVNXuNd67vcCU"
+##UNCOMMENT BEFORE COMMIT##
+# bearer_token = "AAAAAAAAAAAAAAAAAAAAAKvrjQEAAAAAC7rhBPngaUoWZtiE2Po8AI7K0bM%3De9ddHzssMxizr1AxPW4EBmqwKldD8rGDLHnhtXVNXuNd67vcCU"
 
-consumer_key = "llUeaLvBSqztrsxAMisU5YFTc"
-consumer_secret = "hgpOJwGOQxFSxjIwtq5zTMSNnstlTcWfP0CFIsUWJxy26mpphi"
+# consumer_key = "llUeaLvBSqztrsxAMisU5YFTc"
+# consumer_secret = "hgpOJwGOQxFSxjIwtq5zTMSNnstlTcWfP0CFIsUWJxy26mpphi"
 
-access_token = "1593728029452165120-jd6awx3dvx6L6HORTkq99PF1yqsTcS"
-access_token_secret = "57Emarc4sTbb2yWvGZKumjhWZ3S7PAn5nSs4vTPljYFs1"
+# access_token = "1593728029452165120-jd6awx3dvx6L6HORTkq99PF1yqsTcS"
+# access_token_secret = "57Emarc4sTbb2yWvGZKumjhWZ3S7PAn5nSs4vTPljYFs1"
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
 
-@app.route('/twitter_trends')
-def trends():
-    api = tweepy.API(auth)
-    #america trends: 23424977
-    loc = 1
-    locname = 'Earth'
-    #allow user to enter a country, find the corresponding country IDs
-    trends = api.get_place_trends(loc)[0]['trends']
+# @app.route('/twitter_trends')
+# def trends():
+#     api = tweepy.API(auth)
+#     #america trends: 23424977
+#     loc = 1
+#     locname = 'Earth'
+#     #allow user to enter a country, find the corresponding country IDs
+#     trends = api.get_place_trends(loc)[0]['trends']
 
-    #pprint(trends[0]['name'])
+#     #pprint(trends[0]['name'])
 
-    print('Top 5 Trending in '+ locname +': \n', '1. ' + trends[0]['name'] + '\n',
-    '2. ' + trends[1]['name'] + '\n', '3. ' + trends[2]['name'] + '\n', '4. ' + trends[3]['name'] + '\n', '5. ' + trends[4]['name'] + '\n')
+#     print('Top 5 Trending in '+ locname +': \n', '1. ' + trends[0]['name'] + '\n',
+#     '2. ' + trends[1]['name'] + '\n', '3. ' + trends[2]['name'] + '\n', '4. ' + trends[3]['name'] + '\n', '5. ' + trends[4]['name'] + '\n')
 
     
-    return flask.render_template("index.html")
+#     return flask.render_template("index.html")
+##UNCOMMENT BEFORE COMMIT##
 
 @app.route('/abcnews_trends')
 def trends():
@@ -68,25 +73,36 @@ def trends():
     abc = BeautifulSoup(webpage, "html.parser")
 
     title = []
-    for title in abc.find_all("h2"):
-        title.append(title["h2"])
+    for story in abc.find_all("h2"):
+        title.append(story.string)
 
-    href = []
+    links = []
     for link in abc.find_all('a', href=True):
-        href.append(link['href'])
+        links.append(link.string)
 
     #Titles 14-18
     story1title = title[14]
     story2title = title[15]
     story3title = title[16]
-    story3title = title[17]
-    story4title = title[18]
+    story4title = title[17]
+    story5title = title[18]
 
     #Links 16-20
-    story1link = href[16]
-    story2link = href[17]
-    story3link = href[18]
-    story4link = href[19]
-    story5link = href[20]
+    story1link = links[16]
+    story2link = links[17]
+    story3link = links[18]
+    story4link = links[19]
+    story5link = links[20]
+
+    # Creating an HTML file
+    abcpage = open("templates/abcnews_trends.html","w")
+    
+    # Adding input data to the HTML file
+    abcpage.write("<html>\n <body><h3>" + story1title + "</h3>\n<h3>" + story2title + "</h3>\n<h3>" + story3title + "</h3>\n<h3>" + story4title + "</h3>\n<h3>" + story5title + "</h3>\
+            \n</body></html>")
+                
+    # Saving the data into the HTML file
+    abcpage.close()
+
 	
     return flask.render_template("abcnews_trends.html")
